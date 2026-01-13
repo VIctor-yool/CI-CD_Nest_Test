f77f2e17a4217707b4bd95f58aee8c54a345a78f
unknown <sinuur0118@gmail.com>
2026-01-13 15:04:21 +0900

    fixed student_provider

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,29 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"],
+     plugins: { js }, extends: ["js/recommended"],
+      languageOptions: { globals: globals.browser },
+      rules: {
+        // Basic JS rules
+        "no-unused-vars": "off", // 사용하지 않는 변수 감지 비활성화 (아래 TypeScript 전용 규칙 사용)
+        "eqeqeq": ["error", "always"], // ==, != 대신 ===, !== 강제
+        "no-console": "off", // console 사용 허용
+        "curly": "error", // 모든 제어문에서 중괄호 사용 강제
+        "semi": ["off", "always"], // 세미콜론(;) 필수 아님
+        "quotes": ["error", "double"], // 문자열에 쌍따옴표(") 사용 강제
+        "no-var": "error", // var 사용 금지 (let, const 사용 권장)
+        "prefer-const": "error", // 재할당 안 한다면 const 사용 추천
+        "no-undef": "warn", // 선언되지 않은 변수 사용시 경고
+
+        // TypeScript-specific rules
+        "@typescript-eslint/no-unused-vars": [
+          "error", 
+          { "args": "after-used", "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+        ], // 사용하지 않는 변수/매개변수 경고 (단, _로 시작하면 허용)
+        "@typescript-eslint/no-unsafe-assignment": "error", // any 타입 할당 제한
+        "@typescript-eslint/no-unsafe-call": "error", // any 타입 함수 호출 제한
+        "@typescript-eslint/no-unsafe-member-access": "error", // any 타입 객체의 멤버 접근 제한
+      }
+     },
 ]);
--- src/students/students.controller.spec.ts
+++ src/students/students.controller.spec.ts
@@ -1,20 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
 
 describe('StudentsController', () => {
   let controller: StudentsController;
+  let mockRepo: any;
 
   beforeEach(async () => {
+    mockRepo = {
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentsController],
-      providers: [StudentsService],
+      providers: [
+        // studentsService를 실제 서비스로 주입
+        StudentsService,
+        // Student 엔티티의 레포지토리를 mock 객체로 주입
+        { useValue: mockRepo, provide: getRepositoryToken(Student) },
+      ],
     }).compile();
 
     controller = module.get<StudentsController>(StudentsController);
   });
 
-  it('학생 findone get', ()=> {
-    expect(controller.findOne('10')).toBe('학생 10')
-  })
+  it('학생 findone get', () => {
+    expect(controller.findOne('10')).toBe('학생 10');
+  });
 });
